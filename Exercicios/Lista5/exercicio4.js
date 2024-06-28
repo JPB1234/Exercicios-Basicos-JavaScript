e7cb6dd89e20cd5572f75398ea343a7ea2a2f6ce
JPB1234 <jpboechat11@gmail.com>
2024-06-28 17:00:49 -0300

    Término da quinta lista de exercicios

---
+++ Exercicios/Lista5/exercicio4.js
@@ -0,0 +1,4 @@
+//Reforçando conhecimento acerca de arrays e interações
+meuArray = [];
+meuArray.push([1, 2, 3], 'Bom dia', null);
+console.log(meuArray);
---
+++ Exercicios/Lista5/exercicio5.js
@@ -0,0 +1,14 @@
+//Reforçando conhecimento acerca de métodos para modificação de um array
+let clinicaVeterinaria = [];
+
+clinicaVeterinaria.push('Cobra', 'Cachorro', 'Gato');
+console.log(clinicaVeterinaria);
+
+clinicaVeterinaria.pop();
+console.log(clinicaVeterinaria);
+
+clinicaVeterinaria.pop();
+console.log(clinicaVeterinaria);
+
+clinicaVeterinaria.pop();
+console.log(clinicaVeterinaria);

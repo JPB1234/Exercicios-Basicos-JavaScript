5dd52d6386246c579d0d1c2c47b42bca9e2026a7
JPB1234 <jpboechat11@gmail.com>
2024-06-29 12:14:37 -0300

    Finalizando a lista de exercicios 6 e iniciando a sétima lista

--- Exercicios/Lista6/exercicio5.js
+++ Exercicios/Lista6/exercicio5.js
@@ -1,3 +1,19 @@
-//Aprendendo sobre matrizes/arrays multidimensionais
-let valorBase = [];
+//Aprendendo sobre matrizes/arrays multidimensionais e estruturas de repetição
+let matrizVazia = [];
+let valorInicial = 0;
 
+//Estrutura de repetição que forma a matriz tridimensional
+for(let i = 0; i<3; i++){
+    let linha = [];
+    //Estrutura de repetição que insere os vetores em uma matriz bidimensional
+    for(let j = 0; j<3; j++){
+        let coluna = [];
+        //Estrutura de repetição que insere os primeiros elementos dos vetores que irão compor a matriz
+        for(let k = 0; k<3; k++){
+            coluna.push(valorInicial++);
+        }
+        linha.push(coluna);
+    }
+    matrizVazia.push(linha);
+}
+console.log(matrizVazia);
